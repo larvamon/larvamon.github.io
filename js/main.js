2ca4d2c5dd22436e79231d20d5d3bc3bda82aad2
ferryal <Ferryal21@gmail.com>
2018-01-19 14:55:09 +0700

    add fetch genre crime drama

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
 // HORROR - 27
 // CRIME - 80
 // HISTORY - 36
-const actionButton = document.getElementById(`action-button`);
 
 function addAction() {
   const rowAction = $('#row-movie');
@@ -31,4 +30,74 @@ function addAction() {
     });
 }
 
-actionButton.addEventListener(`click`, addAction);
+function addCrime() {
+  const rowAction = $('#row-movie');
+  const url = 'https://api.themoviedb.org/3/genre/80/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data.results);
+      let movies = data.results
+      return movies.map(function(movie) {
+        var node = `
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}" class="movie-poster">
+              </div>
+            </div>
+            `
+        rowAction.append(node);
+      })
+    })
+    .catch(function(error) {
+      console.log(JSON.stringify(error));
+    });
+}
+
+function addDrama() {
+  const rowAction = $('#row-movie');
+  const url = 'https://api.themoviedb.org/3/genre/18/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data.results);
+      let movies = data.results
+      return movies.map(function(movie) {
+        var node = `
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}" class="movie-poster">
+              </div>
+            </div>
+            `
+        rowAction.append(node);
+      })
+    })
+    .catch(function(error) {
+      console.log(JSON.stringify(error));
+    });
+}
+
+function addSciFi() {
+  const rowAction = $('#row-movie');
+  const url = 'https://api.themoviedb.org/3/genre/878/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data.results);
+      let movies = data.results
+      return movies.map(function(movie) {
+        var node = `
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}" class="movie-poster">
+              </div>
+            </div>
+            `
+        rowAction.append(node);
+      })
+    })
+    .catch(function(error) {
+      console.log(JSON.stringify(error));
+    });
+}
