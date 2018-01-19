d2f82f2da253740ee4deafe03db75e9fb2383a02
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-01-19 14:36:38 +0700

    Add fetch movies for action page

--- js/main.js
+++ js/main.js
@@ -5,13 +5,30 @@
 // HORROR - 27
 // CRIME - 80
 // HISTORY - 36
+const actionButton = document.getElementById(`action-button`);
 
-const url = 'https://api.themoviedb.org/3/genre/28/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
-fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    console.log(data.results);
-  })
-  .catch(function(error) {
-    console.log(JSON.stringify(error));
-  });
+function addAction() {
+  const rowAction = $('#row-movie');
+  const url = 'https://api.themoviedb.org/3/genre/28/movies?api_key=6cdc42661a2ffa0286928aa9ded5083e&language=en-US&include_adult=false&sort_by=created_at.asc';
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data.results);
+      let movies = data.results
+      return movies.map(function(movie) {
+        var node = `
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}" class="movie-poster">
+              </div>
+            </div>
+            `
+        rowAction.append(node);
+      })
+    })
+    .catch(function(error) {
+      console.log(JSON.stringify(error));
+    });
+}
+
+actionButton.addEventListener(`click`, addAction);
